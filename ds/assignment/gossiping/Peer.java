f4f7f2b7918293029bfb0ab24eb403f8fee2ac0c
Telmo Ribeiro <telmoribeiro2706@gmail.com>
2023-01-05 19:59:48 +0000

    delivery ready

--- ds/assignment/gossiping/Peer.java
+++ ds/assignment/gossiping/Peer.java
@@ -8,167 +8,294 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 import java.util.logging.FileHandler;
 import java.util.logging.SimpleFormatter;
-import java.util.*; // almost for sure it is Arrays
 
 
 
 public class Peer {
-    static Boolean           hasNewWord;
-    static ArrayList<String> fileList;
-    static ArrayList<String> list;
-    static int[]             table;          // x <=> IP(x) where x is a peer // swap for "String" 
-    int                      hostID;
-    String                   hostAddress;
-    int                      hostPort;
-    Logger                   logger;
+  ArrayList<String>        fileList;
+  HashMap<Integer, String> messageMap;       
+  int                      hostID;
+  String                   hostAddress;
+  String[]                 hostTable;
+  Logger                   logger;
     
 
-    public Peer(int hostID, String hostAddress, int hostPort) {
-        this.hasNewWord  = false;
-        this.fileList    = new ArrayList<String>();
-        this.list        = new ArrayList<String>();
-        this.table       = new int[7];  // swap for "String"
-        this.hostID      = hostID;
-        this.hostAddress = hostAddress;
-        this.hostPort    = hostPort; 
-        this.logger      = Logger.getLogger("logfile");
-        try {
-            FileHandler handler = new FileHandler("./" + this.hostAddress + "_peer.log", true);
-            this.logger.addHandler(handler);
-            SimpleFormatter formatter = new SimpleFormatter();
-            handler.setFormatter(formatter);
-        } catch(Exception exception) { exception.printStackTrace(); }
+  public Peer(int hostID, String hostAddress) {
+    this.fileList    = new ArrayList<String>();
+    this.messageMap  = new HashMap<Integer, String>();
+    this.hostID      = hostID;
+    this.hostAddress = hostAddress;
+    this.hostTable   = new String[7];
+    this.logger      = Logger.getLogger("logfile");
+    try {
+      FileHandler handler = new FileHandler("./" + this.hostAddress + "_peer.log", true);
+      this.logger.addHandler(handler);
+      SimpleFormatter formatter = new SimpleFormatter();
+      handler.setFormatter(formatter);
+    } catch(Exception exception) { exception.printStackTrace(); }
+  }
+
+
+  static private void readCommands(int hostID, String[] hostTable, Logger logger) {
+    Scanner scanner = new Scanner(System.in);
+    String  command = scanner.next();
+    while(!command.equals("end")) {
+      switch(command) {
+        case "register":
+          String serverAddress = scanner.next();
+          System.out.println();
+          handshakeClient(serverAddress, hostID, hostTable, logger);
+          break;
+        default: break;
+      }
+      command = scanner.next();
     }
+  } 
 
-    public static void main(String[] args) throws Exception {
-        int    hostID      = Integer.parseInt(args[0]);
-        String hostAddress = args[1];
-        int    hostPort    = Integer.parseInt(args[2]); 
-        Peer   currPeer    = new Peer(hostID, hostAddress, hostPort);
-        currPeer.logger.info("peer " + hostID + " @ address = " + hostAddress + "\n");
 
-        Scanner fileScanner = new Scanner(new File("wordlist.txt"));
-        while(fileScanner.hasNextLine()){
-            currPeer.fileList.add(fileScanner.nextLine());
-        }
-        fileScanner.close();
+  static private void handshakeClient(String serverAddress, int hostID, String[] hostTable, Logger logger) {
+    try {
+      Socket P2PSocket = new Socket(InetAddress.getByName(serverAddress), 12301);
+      logger.info("handshake_client: new connection to " + P2PSocket.getInetAddress().getHostAddress() + "\n");
 
-        new Thread(new Server(hostID, hostAddress, hostPort, currPeer.logger)).start();
+      BufferedReader P2PIn = new BufferedReader(new InputStreamReader(P2PSocket.getInputStream()));
+      PrintWriter   P2POut = new PrintWriter(P2PSocket.getOutputStream(), true);
+
+      String message = P2PIn.readLine();
+      logger.info("handshake_client: new message from " + P2PSocket.getInetAddress().getHostAddress() + " [message=" + message + "]\n");
+
+      int serverID = Integer.parseInt(message);
+      hostTable[serverID] = serverAddress;
+
+      P2POut.println(hostID);
+      P2POut.flush();
+      
+      P2PSocket.close();
+    } catch(Exception exception) { exception.printStackTrace(); }
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    int    hostID      = Integer.parseInt(args[0]);
+    String hostAddress = args[1]; 
+    Peer   currPeer    = new Peer(hostID, hostAddress);
+    currPeer.logger.info("peer " + hostID + " @ address = " + hostAddress + "\n");
+
+    Scanner scanner = new Scanner(new File("wordlist.txt"));
+    while(scanner.hasNextLine()){
+      currPeer.fileList.add(scanner.nextLine());
+    }
+    scanner.close();
+
+    new Thread(new HandshakeServer(hostID, hostAddress, currPeer.hostTable, currPeer.logger)).start();
+    readCommands(hostID, currPeer.hostTable, currPeer.logger);
+    // Only for testing
+    for (int i = 1; i < 7; i++) {
+      System.out.println("Peer " + i + ": " + currPeer.hostTable[i]);
+    }
+    // Only for testing
+    new Thread(new WordGenerator(currPeer.fileList)).start();
+    new Thread(new Server(hostAddress, currPeer.hostTable, currPeer.messageMap, currPeer.logger)).start();
+    
+    int hostMessages = 0;
+    ServerSocket server = new ServerSocket(12303, 1, InetAddress.getByName("localhost")); 
+    while (true) {
+      try {
+        Socket PWGSocket = server.accept();
+
+        BufferedReader PWGIn = new BufferedReader(new InputStreamReader(PWGSocket.getInputStream()));
+
+        String word    = PWGIn.readLine();
+        String key     = String.valueOf(hostID) + String.valueOf(hostMessages);
+        currPeer.messageMap.put(Integer.parseInt(key), word);
+
+        String message = String.valueOf(hostID) + String.valueOf(hostMessages) + " " + word;
+        new Thread(new Client(message, hostAddress, currPeer.hostTable, currPeer.logger)).start();
+
+        hostMessages++;
+
+        PWGSocket.close();
+      } catch(Exception exception) { exception.printStackTrace(); }
+    }
+  }
+}
+
+
+class Client implements Runnable {
+  String   message;
+  String   hostAddress;
+  String[] hostTable;
+  Logger   logger;
+
+
+  public Client(String message, String hostAddress, String[] hostTable, Logger logger) {
+    this.message     = message;
+    this.hostAddress = hostAddress;
+    this.hostTable   = hostTable;
+    this.logger      = logger;
+  }
+
+  @Override
+  public void run() {
+    Random random = new Random();
+    int    peerID = random.nextInt(7);
+    while(peerID == 0 || hostTable[peerID] == null) {
+      peerID = random.nextInt(7);
+    }
+    String serverAddress = hostTable[peerID];
+    try {
+      Socket P2PSocket = new Socket(InetAddress.getByName(serverAddress), 12302);
+      logger.info("client: new connection to " + P2PSocket.getInetAddress().getHostAddress() + "\n");
+
+      PrintWriter P2POut = new PrintWriter(P2PSocket.getOutputStream(), true);
+
+      P2POut.println(this.message);
+      P2POut.flush();
+
+      P2PSocket.close();
+    } catch(Exception exception) { exception.printStackTrace(); }
+  }
+}
+
+
+class Server implements Runnable{
+  String                    hostAddress;
+  String[]                  hostTable;
+  Logger                    logger;
+  ServerSocket              server;
+  ArrayList<Integer>        blacklist;
+  HashMap<Integer, String>  messageMap;
+
+
+  public Server(String hostAddress, String[] hostTable, HashMap<Integer, String> messageMap, Logger logger) throws Exception {
+    this.hostAddress = hostAddress;
+    this.hostTable   = hostTable;
+    this.messageMap  = messageMap;
+    this.logger      = logger;
+    this.server      = new ServerSocket(12302, 50, InetAddress.getByName(this.hostAddress));
+    this.blacklist   = new ArrayList<Integer>();
+  }
+
+
+  @Override
+  public void run() {
+    while(true) {
+      try {
+        Socket P2PSocket = server.accept();
+        logger.info("server: new connection from " + P2PSocket.getInetAddress().getHostAddress() + "\n");
+
+        BufferedReader P2PIn = new BufferedReader(new InputStreamReader(P2PSocket.getInputStream()));
+
+        String message = P2PIn.readLine();
+        logger.info("server: new message from " + P2PSocket.getInetAddress().getHostAddress() + " [message=" + message + "]\n");
+
+        Scanner scanner = new Scanner(message);
+        int messageID   = Integer.parseInt(scanner.next());
+        String word     = scanner.next();
         
-        Scanner scanner = new Scanner(System.in);
-        String  command = scanner.next();
-        while(!command.equals("end")) {
-            switch(command) {
-                case "register":
-                    try {
-                        int nextPort    = Integer.parseInt(scanner.next());                            // needs to be swaped for nextAddress
-                        Socket PPSocket = new Socket(InetAddress.getByName("localhost"), nextPort);    // localhost needs to be swaped for nextAddress and this port becomes a agreed upon one
-                        currPeer.logger.info("client: new connection to " + PPSocket.getInetAddress().getHostAddress() + "\n");
-
-                        BufferedReader PPIn = new BufferedReader(new InputStreamReader(PPSocket.getInputStream()));
-                        PrintWriter   PPOut = new PrintWriter(PPSocket.getOutputStream(), true);
-
-                        String message = PPIn.readLine();
-                        currPeer.logger.info("client: new message from " + PPSocket.getInetAddress().getHostAddress() + " [message=" + message + "]\n");
-
-                        int nextID = Integer.parseInt(message);
-                        Peer.table[nextID] = nextPort;                                                      // needs to be swaped for nextAddress;
-                        
-                        PPOut.println(currPeer.hostID);
-                        PPOut.flush();
-                    } catch(Exception exception) { exception.printStackTrace(); }
-                    break;
-                default: break;
-            }
-            command = scanner.next();
+        if (!messageMap.containsKey(messageID)) {
+          messageMap.put(messageID, word);
+          // only for testing //
+          System.out.println("Word: " + word + " added to the map with ID: " + messageID);
+          // only for testing //
+          new Thread(new Client(message, this.hostAddress, this.hostTable, this.logger)).start();
         }
-        System.out.println();
-        // Only for testing
-        for (int i = 1; i < 7; i++) {
-            System.out.println("Peer " + i + ": " + Peer.table[i]);
-        }
-        // Only for testing
-
-        new Thread(new wordGenerator()).start();
-
-        while(true) {
-            if (Peer.hasNewWord) {
-                // read and writer already setted
-                // spreads the word through gossiping //
-                Peer.hasNewWord = false;
-            }
-        }
+        else if (!blacklist.contains(messageID) && Math.random() > 0.20) { new Thread(new Client(message, this.hostAddress, this.hostTable, this.logger)).start(); }
+        else                                                            { blacklist.add(messageID); }
+        P2PSocket.close();
+      } catch(Exception exception) { exception.printStackTrace(); }
     }
+  }
 }
 
-class Server implements Runnable {
-    int          hostID;
-    String       hostAddress;
-    int          hostPort;
-    Logger       logger;
-    ServerSocket server;
 
-    Server(int hostID, String hostAddress, int hostPort, Logger logger) throws Exception {
-        this.hostID      = hostID;
-        this.hostAddress = hostAddress;
-        this.hostPort    = hostPort;
-        this.logger      = logger;
-        server           = new ServerSocket(this.hostPort, 5, InetAddress.getByName(this.hostAddress));
-    }
+class HandshakeServer implements Runnable {
+  int          hostID;
+  String       hostAddress;
+  String[]     hostTable;
+  Logger       logger;
+  ServerSocket handshakeServer;
 
-    @Override
-    public void run() {
-        while(true) {
-            try {
-                Socket PPSocket = server.accept();
-                logger.info("server: new connection from " + PPSocket.getInetAddress().getHostAddress() + "\n");
 
-                BufferedReader PPIn = new BufferedReader(new InputStreamReader(PPSocket.getInputStream()));
-                PrintWriter   PPOut = new PrintWriter(PPSocket.getOutputStream(), true);
+  public HandshakeServer(int hostID, String hostAddress, String[] hostTable, Logger logger) throws Exception {
+    this.hostID          = hostID;
+    this.hostAddress     = hostAddress;
+    this.hostTable       = hostTable;
+    this.logger          = logger;
+    this.handshakeServer = new ServerSocket(12301, 5, InetAddress.getByName(this.hostAddress));
+  }
 
-                PPOut.println(String.valueOf(this.hostID));
-                PPOut.flush();
 
-                String message = PPIn.readLine();
-                logger.info("server: new message from " + PPSocket.getInetAddress().getHostAddress() + " [message=" + message + "]\n");
+  @Override
+  public void run() {
+    while(true) {
+      try {
+        Socket P2PSocket = handshakeServer.accept();
+        logger.info("handshake_server: new connection from " + P2PSocket.getInetAddress().getHostAddress() + "\n");
 
-                int nextID = Integer.parseInt(message);
-                Peer.table[nextID] = PPSocket.getPort();   // swap getPort for getInetAddress().getHostAddress()
-                
-                // escerve a socket que descobriu num array global de sockets
-                // pode terminar e comecar a conexao ou reaproveitar e nao precisar do array de sockets
-            } catch(Exception exception) { exception.printStackTrace(); }
-        }   
-    }
+        BufferedReader P2PIn = new BufferedReader(new InputStreamReader(P2PSocket.getInputStream()));
+        PrintWriter   P2POut = new PrintWriter(P2PSocket.getOutputStream(), true);
+
+        P2POut.println(String.valueOf(this.hostID));
+        P2POut.flush();
+
+        String message = P2PIn.readLine();
+        logger.info("handshake_server: new message from " + P2PSocket.getInetAddress().getHostAddress() + " [message=" + message + "]\n");
+
+        int clientID = Integer.parseInt(message);
+        hostTable[clientID] = P2PSocket.getInetAddress().getHostAddress();
+
+        P2PSocket.close();
+      } catch(Exception exception) { exception.printStackTrace(); }
+    }   
+  }
 }
 
-class wordGenerator implements Runnable {
-    public void generate() {
-        Random random = new Random();
-        String word   = Peer.fileList.get(random.nextInt(Peer.fileList.size()));
-        System.out.println("new word: " + word);    // remove when it is finished
-        Peer.list.add(word);
-        Peer.hasNewWord = true;
-    }
 
-    @Override
-    public void run() {
-        PoissonProcess process = new PoissonProcess(2, new Random());
-        while(true) {
-            double dt = process.timeForNextEvent() * 60 * 1000;
-            long time = (long)dt;
-            System.out.println("Poisson Value: " + dt); // remove when it is finished
-            System.out.println("t - " + time);          // remove when it is finished
-            try {
-                Thread.sleep(time);
-            } catch(Exception exception) { exception.printStackTrace(); }
-            generate();
-        }
+
+class WordGenerator implements Runnable {
+  ArrayList<String> fileList;
+
+  public WordGenerator(ArrayList<String> fileList) {
+    this.fileList = fileList;
+  }
+
+  public void generate(ArrayList<String> fileList) {
+    Random random = new Random();
+    String word   = fileList.get(random.nextInt(fileList.size()));
+    System.out.println("Generated Word: " + word);    // REMOVE WHEN FINISHED //
+
+    try {    
+      Socket WGPSocket = new Socket(InetAddress.getByName("localhost"), 12303);
+
+      PrintWriter WGPOut = new PrintWriter(WGPSocket.getOutputStream(), true);
+
+      WGPOut.println(word);
+      WGPOut.flush();
+
+      WGPOut.close();
+    } catch(Exception exception) { exception.printStackTrace(); } 
+  }
+
+  @Override
+  public void run() {
+    PoissonProcess process = new PoissonProcess(2, new Random());
+    while(true) {
+      double dt = process.timeForNextEvent() * 60 * 1000;
+      long time = (long)dt;
+      System.out.println("Poisson Value: " + dt);   // REMOVE WHEN FINISHED //
+      System.out.println("t - " + time);            // REMOVE WHEN FINISHED // 
+      try { Thread.sleep(time); } catch(Exception exception) { exception.printStackTrace(); }
+      generate(this.fileList);
     }
+  }
 }
 
 
